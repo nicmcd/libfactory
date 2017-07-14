bd0f82c223ac3d905b54cfa1aa857df314aa4c66
Nic McDonald <n.mcdonald83@gmail.com>
2017-07-13 21:35:52 -0600

    now using NOTICE file

--- src/factory/Factory.h
+++ src/factory/Factory.h
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) 2017, Nic McDonald, Doug Gibson
- * All rights reserved.
- *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
@@ -16,6 +13,9 @@
  * endorse or promote products derived from this software without specific prior
  * written permission.
  *
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,10 +27,6 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
- * A significant portion of this work was modified from:
- * http://stackoverflow.com/questions/2906638
- *
  */
 #ifndef FACTORY_FACTORY_H_
 #define FACTORY_FACTORY_H_
